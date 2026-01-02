0df0dc967f1b32897574141523feb22925766cc0
EDWIN CHACKO <163020056+chackochii@users.noreply.github.com>
2026-01-02 18:48:34 +0530

    Add route to get project user task hours

--- routes/taskTimeLogRoutes.js
+++ routes/taskTimeLogRoutes.js
@@ -1,9 +1,10 @@
 import express from "express"; 
-import { getUserWorkHours } from "../modules/taskTimeLog/taskTimeLogController.js";
+import { getUserWorkHours, getProjectUserTaskHours } from "../modules/taskTimeLog/taskTimeLogController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/user-one-day/:userId", authMiddleware, getUserWorkHours);
+router.get("/:projectId", authMiddleware, getProjectUserTaskHours)
 
 export default router;
