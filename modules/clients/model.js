8c21b1518c63919840e570f72f6b56f2ad755a49
EDWIN CHACKO <163020056+chackochii@users.noreply.github.com>
2026-01-02 15:03:39 +0530

    Create Client model with Sequelize definitions
    Define the Client model with various attributes including id, name, email, phone, address, amount, and status.

--- modules/clients/model.js
+++ modules/clients/model.js
@@ -1,1 +1,45 @@
-// model.js
+import { DataTypes } from "sequelize";
+
+export default (sequelize) => {
+  const Client = sequelize.define(
+    "Client",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      phone: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      address: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      amount: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false,
+        defaultValue: 0.0,
+      },
+      status: {
+        type: DataTypes.ENUM("active", "inactive"),
+        defaultValue: "active",
+      },
+    },
+    {
+      tableName: "clients",
+      timestamps: true,
+    }
+  );
+
+  return Client;
+};
