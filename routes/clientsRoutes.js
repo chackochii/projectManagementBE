2549f9be6b2e27023066aa7105a4ac64f9a5f2cf
EDWIN CHACKO <163020056+chackochii@users.noreply.github.com>
2026-01-02 14:56:07 +0530

    Add client routes with authentication middleware

--- routes/clientsRoutes.js
+++ routes/clientsRoutes.js
@@ -1,1 +1,10 @@
-// dummy routes
+import express from "express";
+import { addClient, getClients } from "../modules/clients/clients.controller.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.get("/",  getClients);
+router.post("/", authMiddleware, addClient);
+
+export default router;
