03ed4233a37824e9e672bde697c9639177292d51
EDWIN CHACKO <163020056+chackochii@users.noreply.github.com>
2026-01-16 04:34:37 +0530

    Add getUserTasksReport route to taskRoutes

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,6 +1,6 @@
 // routes/taskRoutes.js
 import express from "express";
-import { createTask, getBacklogTasks, updateTaskStatus, getTasksByStatus, getActiveTasksForUser, startTask, stopTask, getMonthlyReport,getUserTasksController, assignTask, unassignTask} from "../modules/task/taskController.js";
+import { createTask, getBacklogTasks, updateTaskStatus, getTasksByStatus, getActiveTasksForUser, startTask, stopTask, getMonthlyReport,getUserTasksController, assignTask, unassignTask, getUserTasksReport} from "../modules/task/taskController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
@@ -11,8 +11,8 @@ router.patch("/status", authMiddleware, updateTaskStatus);
 router.get("/status/:status/:projectId", authMiddleware, getTasksByStatus);
 
 router.get("/my-active-tasks/:projectId", authMiddleware, getActiveTasksForUser);
-router.post("/start/:taskId",authMiddleware, startTask);
-router.post("/end/:taskId",authMiddleware, stopTask);
+router.post("/start/:taskId", authMiddleware, startTask);
+router.post("/end/:taskId", authMiddleware, stopTask);
 
 router.get("/monthly", authMiddleware, getMonthlyReport);
 
@@ -20,6 +20,9 @@ router.get("/user-tasks/:projectId", authMiddleware, getUserTasksController);
 
 router.patch("/assign", authMiddleware, assignTask);
 router.patch("/unassign", authMiddleware, unassignTask);
+
+router.get("/user-tasks", authMiddleware, getUserTasksReport );
+
 
 
 
