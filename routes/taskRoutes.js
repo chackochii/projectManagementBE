e80cc252778d0fcc48a1dd6cb14a14e2f84dc0fc
EDWIN CHACKO <163020056+chackochii@users.noreply.github.com>
2026-02-18 00:31:40 +0530

    Add route for fetching user tasks full details

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,6 +1,6 @@
 // routes/taskRoutes.js
 import express from "express";
-import { createTask, getBacklogTasks, updateTaskStatus, getTasksByStatus, getActiveTasksForUser, startTask, stopTask, getMonthlyReport,getUserTasksController, assignTask, unassignTask, getUserTasksReport} from "../modules/task/taskController.js";
+import { createTask, getBacklogTasks, updateTaskStatus, getTasksByStatus, getActiveTasksForUser, startTask, stopTask, getMonthlyReport,getUserTasksController, assignTask, unassignTask, getUserTasksReport, getUserTasksFullDetailsController} from "../modules/task/taskController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
@@ -22,6 +22,8 @@ router.patch("/assign", authMiddleware, assignTask);
 router.patch("/unassign", authMiddleware, unassignTask);
 
 router.get("/user-tasks", authMiddleware, getUserTasksReport );
+
+router.get("/tasksDetails", authMiddleware, getUserTasksFullDetailsController )
 
 
 
