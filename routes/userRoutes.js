9cb56045bdbc10e4568536614ffcae7593c86fd3
EDWIN CHACKO <163020056+chackochii@users.noreply.github.com>
2026-03-14 21:44:10 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,121 @@
 import express from "express";
-import {loginUser, createUser , getUsers, loginAdmin, updateUserStatus,updateUser, deleteUser} from "../modules/user/user.controller.js";
+import {loginUser, createUser , getUsers, loginAdmin, updateUserStatus,updateUser, deleteUser, clientLoginUser} from "../modules/user/user.controller.js";
 import {requireAdmin} from "../middleware/roleMiddleware.js";
 import {authMiddleware} from "../middleware/authMiddleware.js";
 
 export const userRoutes = () => {
   const router = express.Router();
 
+  /**
+ * @swagger
+ * components:
+ *   schemas:
+ *     LoginRequest:
+ *       type: object
+ *       required:
+ *         - email
+ *         - password
+ *       properties:
+ *         email:
+ *           type: string
+ *           example: user@example.com
+ *         password:
+ *           type: string
+ *           example: password123
+ *
+ *     AuthUser:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *           example: 5
+ *         name:
+ *           type: string
+ *           example: Edwin Chacko
+ *         email:
+ *           type: string
+ *           example: edwin@example.com
+ *         role:
+ *           type: string
+ *           example: developer
+ *         status:
+ *           type: string
+ *           example: active
+ *
+ *     LoginResponse:
+ *       type: object
+ *       properties:
+ *         message:
+ *           type: string
+ *           example: Login successful
+ *         token:
+ *           type: string
+ *           example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...
+ *         user:
+ *           $ref: '#/components/schemas/AuthUser'
+ */
+
+
+
+  /**
+ * @swagger
+ * /users/login:
+ *   post:
+ *     summary: Login user
+ *     tags: [Authentication]
+ *     description: Login endpoint for internal users such as admin, developer, project manager etc.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/LoginRequest'
+ *     responses:
+ *       200:
+ *         description: Login successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/LoginResponse'
+ *       401:
+ *         description: Invalid email or password
+ */
+
   router.post("/login",loginUser);
+
+  /**
+ * @swagger
+ * /users/client-login:
+ *   post:
+ *     summary: Client login
+ *     tags: [Authentication]
+ *     description: Login endpoint only for users with role "client"
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/LoginRequest'
+ *     responses:
+ *       200:
+ *         description: Client login successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Client login successful
+ *                 token:
+ *                   type: string
+ *                 user:
+ *                   $ref: '#/components/schemas/AuthUser'
+ *       401:
+ *         description: Invalid credentials or user not a client
+ */
+
+  router.post("/client-login", clientLoginUser);
   // router.post("/register",authMiddleware, requireAdmin(), createUser);
   router.post("/register", createUser);
   router.get("/", authMiddleware, getUsers);
